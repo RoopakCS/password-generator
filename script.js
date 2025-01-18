e7322b25fd22f63a2d463375e0c579aa5c62e6ed
RoopakCS <roopaksenthilkumar@gmail.com>
2025-01-18 18:08:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// API = "https://api.genratr.com/?length=16&uppercase&lowercase&special&numbers";
+
+const inputField = document.querySelector("input");
+const copybutton = document.getElementById("copy-button");
+
+async function generatePassword() {
+  const response = await fetch(
+    "https://api.genratr.com/?length=16&uppercase&lowercase&special&numbers"
+  );
+  const result = await response.json();
+  inputField.value = result.password;
+}
+
+copybutton.addEventListener("click", function () {
+  navigator.clipboard.writeText(inputField.value).then(
+    alert("Copied to clipboard")
+  )
+});
+
